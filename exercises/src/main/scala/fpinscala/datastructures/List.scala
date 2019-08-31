724b561daad95326081e52075695875d5d23913c
Vladimir K <illfaku@gmail.com>
2019-08-31 18:27:23 +0600

    Resolved exercise 3.5

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -70,7 +70,13 @@ object List { // `List` companion object. Contains functions for creating and wo
     } else l
   }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @annotation.tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => l
+    case Cons(h, tail) =>
+      if (f(h)) dropWhile(tail, f)
+      else l
+  }
 
   def init[A](l: List[A]): List[A] = ???
 
