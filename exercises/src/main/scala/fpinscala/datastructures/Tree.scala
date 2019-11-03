9f0c8a7ce386e70ed2ea8148a7062eeded6eba92
Vladimir K <illfaku@gmail.com>
2019-11-03 20:30:55 +0600

    Resolved exercise 3.25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -4,10 +4,20 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-
 object Tree {
 
+  def size(t: Tree[_]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
-
-
+  def size2(t: Tree[_]): Int = {
+    @annotation.tailrec
+    def loop(branches: List[Tree[_]], result: Int): Int = branches match {
+      case Nil => result
+      case Cons(Leaf(_), tail) => loop(tail, result + 1)
+      case Cons(Branch(l, r), tail) => loop(Cons(l, Cons(r, tail)), result + 1)
+    }
+    loop(List(t), 0)
+  }
 }
