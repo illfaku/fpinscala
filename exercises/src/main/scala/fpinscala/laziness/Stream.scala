dd683699b73f85c396af96bc94f686fb5976d05e
Vladimir K <illfaku@gmail.com>
2019-11-12 19:43:38 +0600

    Resolved exercise 5.3

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -54,7 +54,10 @@ trait Stream[+A] {
     case _ => this
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => Stream.cons(h(), t().takeWhile(p))
+    case _ => Stream.empty
+  }
 
   def forAll(p: A => Boolean): Boolean = ???
 
