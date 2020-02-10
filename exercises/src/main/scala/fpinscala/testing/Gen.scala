857bcd31d051f47ddc2b2011fac4f85325a7f05e
Vladimir K <illfaku@gmail.com>
2020-02-10 11:26:43 +0300

    Resolved exercise 8.10

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -13,34 +13,6 @@ The library developed in this chapter goes through several iterations. This file
 shell, which you can fill in and modify while working through the chapter.
 */
 
-case class GenState[A](sample: State[RNG,A]) {
-
-  def flatMap[B](f: A => GenState[B]): GenState[B] = GenState(sample.flatMap(f(_).sample))
-
-  def listOfN(size: GenState[Int]): GenState[List[A]] = size.flatMap(GenState.listOfN(_, this))
-}
-
-object GenState {
-
-  def choose(start: Int, stopExclusive: Int): GenState[Int] = {
-    require(start >= 0 && start < stopExclusive)
-    GenState(State(RNG.nonNegativeLessThan(stopExclusive - start)).map(start + _))
-  }
-
-  def unit[A](a: => A): GenState[A] = GenState(State.unit(a))
-
-  def boolean: GenState[Boolean] = GenState(State[RNG, Int](_.nextInt).map(_ % 2 == 1))
-
-  def listOfN[A](n: Int, g: GenState[A]): GenState[List[A]] = GenState(State.sequence(List.fill(n)(g.sample)))
-
-  def union[A](g1: GenState[A], g2: GenState[A]): GenState[A] = boolean.flatMap(if (_) g1 else g2)
-
-  def weighted[A](g1: (GenState[A],Double), g2: (GenState[A],Double)): GenState[A] = {
-    val x = g1._2.abs / (g1._2.abs + g2._2.abs)
-    GenState(State(RNG.double)).flatMap(d => if (d < x) g1._1 else g2._1)
-  }
-}
-
 case class Prop(run: (TestCases,RNG) => Result) {
   def &&(p: Prop): Prop = Prop((t, r) => {
     val result = run(t, r)
@@ -74,15 +46,33 @@ object Prop {
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    require(start >= 0 && start < stopExclusive)
+    Gen(State(RNG.nonNegativeLessThan(stopExclusive - start)).map(start + _))
+  }
+
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] = Gen(State[RNG, Int](_.nextInt).map(_ % 2 == 1))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(State.sequence(List.fill(n)(g.sample)))
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(if (_) g1 else g2)
+
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val x = g1._2.abs / (g1._2.abs + g2._2.abs)
+    Gen(State(RNG.double)).flatMap(d => if (d < x) g1._1 else g2._1)
+  }
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+case class Gen[A](sample: State[RNG,A]) {
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(f(_).sample))
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(Gen.listOfN(_, this))
+
+  def unsized: SGen[A] = SGen(_ => this)
 }
 
-trait SGen[+A] {
-
-}
-
+case class SGen[+A](forSize: Int => Gen[A])
