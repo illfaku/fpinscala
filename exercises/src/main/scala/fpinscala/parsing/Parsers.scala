a6e347676999ac83c28166b094dc89a3cda698c3
Vladimir K <illfaku@gmail.com>
2020-02-10 20:09:21 +0300

    Resolved exercise 9.1

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,12 +1,27 @@
 package fpinscala.parsing
 
-import language.higherKinds
-
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+  def slice[A](p: Parser[A]): Parser[String]
+  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)]
+
+  def map2[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] = product(p, p2).map(f.tupled)
+  def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_ :: _)
 
   case class ParserOps[A](p: Parser[A]) {
 
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
+    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
 
+    def many: Parser[List[A]] = self.many(p)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
   }
 
   object Laws {
